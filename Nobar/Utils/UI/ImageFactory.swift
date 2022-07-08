f39897df5befbe28ba303304b672c1c703ffdfbe
Ian <ian@channel.io>
2022-07-09 02:13:02 +0900

    [ADD] ImageUtil 추가

---
+++ Nobar/Utils/UI/ImageFactory.swift
@@ -0,0 +1,14 @@
+//
+//  ImageFactory.swift
+//  Nobar
+//
+//  Created by Ian on 2022/07/09.
+//
+
+import UIKit
+
+enum ImageFactory {
+  static var someImage: UIImage? {
+    UIImage(named: "someImage")
+  }
+}
