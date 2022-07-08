e65e853615cf564268c1ffaf7373a72c7219d848
Ian <ian@channel.io>
2022-07-08 12:33:06 +0900

    [ADD] base extensions : UICollectionView, UIStoryboard
    # 제목의 길이는 최대 50글자까지 한글로 간단 명료하게 작성 
    # 제목을 작성하고 반드시 빈 줄 한 줄을 만들어야 함 
    # 제목에 .(마침표) 생략 
    # <label> 리스트 
    # - Feature : 새로운 기능 
    # - Fix : 버그 수정 
    # - Improvement : 기능 or 퀄리티 향상
    # - Release : 릴리즈 관련
    # - Etc : 기타 변경사항
    
    [description]
    # 내용의 길이는 한 줄당 72글자 내외에서 줄 바꿈. 한글로 간단 명료하게 작성 
    # 어떻게 보다는 무엇을, 왜 변경했는지를 작성할 것 (필수)

---
+++ Nobar/GlobalExtensions/UICollectionView+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UICollectionView+Extensions.swift
+//  Nobar
+//
+//  Created by Ian on 2022/07/08.
+//
+
+import UIKit
+
+extension UICollectionView {
+  func dequeueReusableCell<T>(
+    ofType cellType: T.Type = T.self,
+    at indexPath: IndexPath
+  ) -> T where T: UICollectionViewCell {
+    guard
+      let cell = dequeueReusableCell(withReuseIdentifier: cellType.identifier, for: indexPath) as? T
+    else {
+      return T()
+    }
+    
+    return cell
+  }
+}
+
