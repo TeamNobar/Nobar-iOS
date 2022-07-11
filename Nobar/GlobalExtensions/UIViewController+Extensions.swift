ca25b2ff6912ae437330092e38cf348d89be8c16
Yujin Lee <yj587878@gmail.com>
2022-07-11 22:50:07 +0900

    [ADD] addSubviews 함수 추가

--- Nobar/GlobalExtensions/UIViewController+Extensions.swift
+++ Nobar/GlobalExtensions/UIViewController+Extensions.swift
@@ -5,4 +5,12 @@
 //  Created by Ian on 2022/07/06.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { self.addSubview($0) }
+    }
+  
+}
