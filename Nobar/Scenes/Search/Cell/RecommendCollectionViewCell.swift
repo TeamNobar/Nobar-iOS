117e8f5d824d131b7b4a3b83748e96ba04bae727
suyeon <rlatndus9911@naver.com>
2022-07-13 03:51:35 +0900

    [FEAT] 추천검색어 Cell 구현 (#11)

---
+++ Nobar/Scenes/Search/Cell/RecommendCollectionViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  RecommendCollectionViewCell.swift
+//  Nobar
+//
+//  Created by 김수연 on 2022/07/12.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+
+  private let numberLabel = UILabel().then {
+    $0.text = "1"
+    $0.textColor = .black
+    $0.font = Pretendard.size15.regular()
+  }
+
+  private let keywordLabel = UILabel().then {
+    $0.text = "추천 검색어 어쩌구"
+    $0.textColor = .black
+    $0.font = Pretendard.size15.regular()
+  }
+
+  private let upImageView = UIImageView().then {
+    $0.image = ImageFactory.icnUp
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    render()
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func prepareForReuse() {
+    keywordLabel.text = ""
+  }
+
+  private func render() {
+    addSubview(numberLabel)
+    numberLabel.snp.makeConstraints {
+      $0.leading.equalToSuperview()
+      $0.top.bottom.equalToSuperview().inset(16)
+    }
+
+    addSubview(keywordLabel)
+    keywordLabel.snp.makeConstraints {
+      $0.leading.equalTo(numberLabel.snp.trailing).offset(16)
+      $0.top.bottom.equalToSuperview().inset(16)
+    }
+
+    addSubview(upImageView)
+    upImageView.snp.makeConstraints {
+      $0.width.equalTo(22)
+      $0.height.equalTo(24)
+      $0.top.equalToSuperview().inset(13)
+      $0.trailing.equalToSuperview().inset(21)
+    }
+  }
+
+  func update(data: SearchModel) {
+    numberLabel.text = "\(data.order)"
+    keywordLabel.text = data.recommendKeyword
+  }
+}
