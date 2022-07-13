a662c646e8d31a2dade00087d7c6f7b39d8d9c0b
Yujin Lee <yj587878@gmail.com>
2022-07-13 23:51:57 +0900

    [ ADD] 이미지 에셋 추가(#21)

--- Nobar/Resources/Utils/ImageFactory.swift
+++ Nobar/Resources/Utils/ImageFactory.swift
@@ -11,4 +11,21 @@ enum ImageFactory {
   static var someImage: UIImage? {
     UIImage(named: "someImage")
   }
+  
+    static var logo: UIImage? {
+      UIImage(named: "logo")
+    }
+    
+    static var icnTag: UIImage? {
+      UIImage(named: "icnTag")
+    }
+    
+    static var dummy1: UIImage? {
+      UIImage(named: "dummy1")
+    }
+    
+    static var dummy2: UIImage? {
+      UIImage(named: "dummy2")
+    }
+    
 }
