f66ae4299d33e3ed5e478328178fe448f0ecbc15
Yujin Lee <yj587878@gmail.com>
2022-07-15 01:13:42 +0900

    [REFACTOR] 섹션별 레이아웃 함수로 분리

--- Nobar/Scenes/Main/MainViewController.swift
+++ Nobar/Scenes/Main/MainViewController.swift
@@ -11,7 +11,7 @@ import Then
 import SnapKit
 
 final class MainViewController: BaseViewController {
-
+  
   private let logoView = UIView().then {
     $0.backgroundColor = .white
   }
@@ -21,92 +21,98 @@ final class MainViewController: BaseViewController {
   }
   
   private lazy var homeCollectionView: UICollectionView = {
-       let layout = getLayout()
-       layout.configuration.interSectionSpacing = 0
-
-       let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-       collectionView.showsHorizontalScrollIndicator = false
-       collectionView.showsVerticalScrollIndicator = false
+    let layout = getLayout()
+    layout.configuration.interSectionSpacing = 0
+    
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.showsHorizontalScrollIndicator = false
+    collectionView.showsVerticalScrollIndicator = false
     collectionView.register(CocktailCVC.self, forCellWithReuseIdentifier: CocktailCVC.identifier)
-       return collectionView
+    return collectionView
   }()
   
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
-  override func setupConstraints() {
-    <#code#>
-  }
 }
 
 extension MainViewController {
+  private func getArchiveSectionLayout() -> NSCollectionLayoutSection {
+    //        let itemInset: CGFloat = 2.5
+    
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(0.5),
+      heightDimension: .fractionalHeight(1)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4.5, bottom: 0, trailing: 4.5)
+    
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1.0),
+      heightDimension: .estimated(120.0)
+    )
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    
+    var section = NSCollectionLayoutSection(group: group)
+    section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 26, bottom: 12, trailing: 26)
+    
+    section = self.addHeaderView(section: section)
+    
+    return section
+  }
+  private func getGuideSectionLayout() -> NSCollectionLayoutSection {
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1.0),
+      heightDimension: .fractionalHeight(1.0)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .absolute(172),
+      heightDimension: .absolute(138)
+    )
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    
+    var section = NSCollectionLayoutSection(group: group)
+    section.interGroupSpacing = 12
+    section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 26, bottom: 12, trailing: 26)
+    
+    section = self.addHeaderView(section: section)
+    
+    return section
+  }
+  
+  private func getRecommendSectionLayout() -> NSCollectionLayoutSection {
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1.0),
+      heightDimension: .fractionalHeight(1.0)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .absolute(172),
+      heightDimension: .absolute(138)
+    )
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+    
+    var section = NSCollectionLayoutSection(group: group)
+    section.interGroupSpacing = 12
+    section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 26, bottom: 12, trailing: 26)
+    
+    section = self.addHeaderView(section: section)
+    
+    return section
+  }
+  
   private func getLayout() -> UICollectionViewCompositionalLayout {
     UICollectionViewCompositionalLayout { (section, env) -> NSCollectionLayoutSection? in
       switch section {
-      case 0:
-//        let itemInset: CGFloat = 2.5
+      case 0: return self.getArchiveSectionLayout()
+      case 1: return self.getGuideSectionLayout()
+      default: return self.getRecommendSectionLayout()
         
-        let itemSize = NSCollectionLayoutSize(
-          widthDimension: .fractionalWidth(0.5),
-          heightDimension: .fractionalHeight(1)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4.5, bottom: 0, trailing: 4.5)
-        
-        let groupSize = NSCollectionLayoutSize(
-          widthDimension: .fractionalWidth(1.0),
-          heightDimension: .estimated(120.0)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        var section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 26, bottom: 12, trailing: 26)
-        
-        section = self.addHeaderView(section: section)
-        
-        return section
-      case 1:
-        let itemSize = NSCollectionLayoutSize(
-          widthDimension: .fractionalWidth(1.0),
-          heightDimension: .fractionalHeight(1.0)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(
-          widthDimension: .absolute(172),
-          heightDimension: .absolute(138)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        var section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 12
-        section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 26, bottom: 12, trailing: 26)
-        
-        section = self.addHeaderView(section: section)
-        
-        return section
-      default:
-        let itemSize = NSCollectionLayoutSize(
-          widthDimension: .fractionalWidth(1.0),
-          heightDimension: .absolute(80)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(
-          widthDimension: .fractionalWidth(1.0),
-          heightDimension: .estimated(456)
-        )
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 5)
-        
-        var section = NSCollectionLayoutSection(group: group)
-
-        section.orthogonalScrollingBehavior = .continuous
-        section = self.addHeaderView(section: section)
-        
-        return section
-      
       }
     }
   }
