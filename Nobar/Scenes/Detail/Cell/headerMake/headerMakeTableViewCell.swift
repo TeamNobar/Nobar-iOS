bd43e8b43b06ddb4af38de0af62d6adfbb309b99
hansua <hansua990928@gmail.com>
2022-07-17 03:45:33 +0900

    [ADD] 상세뷰 header 추가

---
+++ Nobar/Scenes/Detail/Cell/headerMake/headerMakeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  headerMakeTableViewCell.swift
+//  Nobar
+//
+//  Created by Sua Han on 2022/07/17.
+//
+
+import UIKit
+
+class headerMakeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
