711b9095501f24773776bb7b33d948d64b6b7f0d
Yujin Lee <yj587878@gmail.com>
2022-07-17 14:57:19 +0900

    [ADD] Image Factory 프로퍼티 추가(#31)

--- Nobar/Resources/Utils/ImageFactory.swift
+++ Nobar/Resources/Utils/ImageFactory.swift
@@ -16,4 +16,7 @@ enum ImageFactory {
   static var icnSearch: UIImage? { UIImage(named: "icnSearch") }
   static var icnUp: UIImage? { UIImage(named: "icnUp") }
   static var icnX: UIImage? { UIImage(named: "icnX") }
+  static var btnCancel: UIImage? { UIImage(named: "btnCancel") }
+  static var btnScoreEmpty: UIImage? { UIImage(named: "btnScoreEmpty") }
+  static var btnScoreFill: UIImage? { UIImage(named: "btnScoreFill") }
 }
