c7aaa3da516b3e79430863ce22f5105eba142b04
hansua <hansua990928@gmail.com>
2022-07-18 02:19:52 +0900

    [ADD] identifier 추가

--- Nobar/Scenes/Detail/DetailViewController.swift
+++ Nobar/Scenes/Detail/DetailViewController.swift
@@ -11,7 +11,15 @@ final class DetailViewController: BaseViewController {
     @IBOutlet weak var cocktailNameHeaderView: UIView!
     @IBOutlet weak var detailTableView: UITableView!
 
-    let identifiers = [InfoTableViewCell.identifier, HeaderReadyTableViewCell.identifier, IngredientsTableViewCell.identifier ]
+    let identifiers = [
+        InfoTableViewCell.identifier,
+        HeaderReadyTableViewCell.identifier,
+        IngredientsTableViewCell.identifier,
+        HeaderMakeTableViewCell.identifier,
+        StepsTableViewCell.identifier,
+        HeaderRecordTableViewCell.identifier,
+        RecordTableViewCell.identifier
+    ]
 
   override func viewDidLoad() {
     super.viewDidLoad()
