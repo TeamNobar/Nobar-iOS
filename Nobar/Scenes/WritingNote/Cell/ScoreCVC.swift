30eb529a46454fff43e2190ed93384c3adfce869
Yujin Lee <yj587878@gmail.com>
2022-07-19 04:10:13 +0900

    [FEAT] ScoreCVC 초기 구현(#31)

--- Nobar/Scenes/WritingNote/Cell/ScoreCVC.swift
+++ Nobar/Scenes/WritingNote/Cell/ScoreCVC.swift
@@ -8,5 +8,44 @@
 import UIKit
 
 final class ScoreCVC: UICollectionViewCell {
-    
+  
+  private let slider = UISlider()
+  
+  private var heartImageViews: [UIImageView] = []
+  
+  private var heartStackView = UIStackView().then{
+    $0.axis = .horizontal
+    $0.spacing = 21
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
 }
+
+// MARK: - UI & Layout
+
+extension ScoreCVC {
+  
+  private func render() {
+    addSubview(heartStackView)
+    
+  }
+  
+  private func initStackView() {
+    
+    for i in 0..<5 {
+      let imageView = UIImageView(image:ImageFactory.btnScoreFill)
+      heartStackView.addArrangedSubview(imageView)
+    }
+  }
+  
+}
+
+
