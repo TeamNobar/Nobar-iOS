ced64897db517a8e2af8b3f14a82ed3bf813103b
Yujin Lee <yj587878@gmail.com>
2022-07-19 02:21:39 +0900

    [FEAT] 날짜 헤더뷰 추가(#31)

--- Nobar/Scenes/WritingNote/Cell/TastingNoteHeaderView.swift
+++ Nobar/Scenes/WritingNote/Cell/TastingNoteHeaderView.swift
@@ -10,15 +10,21 @@ import UIKit
 final class TastingNoteHeaderView: UICollectionReusableView {
   enum TastingNoteHeaderType {
     case cocktail
+    case date
     case taste
     case score
     case evaluation
     case experience
   }
   
-  private var titleLabel = UILabel().then {
+  private let titleLabel = UILabel().then {
     $0.textColor = Color.navy01.getColor()
     $0.font = Pretendard.size20.black()
+  }
+  
+  private let subLabel = UILabel().then {
+    $0.textColor = Color.pink01.getColor()
+    $0.font = Pretendard.size10.medium()
   }
   
   override init(frame: CGRect) {
@@ -47,6 +53,8 @@ extension TastingNoteHeaderView {
     switch type {
     case .cocktail:
       titleLabel.text = "기록하고 싶은 칵테일을 알려주세요"
+    case .date:
+      titleLabel.text = "칵테일을 만든 날짜를 알려주세요"
     case .taste:
       titleLabel.text = "맛은 어땠나요?"
     case .score:
