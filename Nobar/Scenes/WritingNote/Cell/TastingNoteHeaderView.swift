53a57f5fd4dc732a466934346cf870e09a0f9fce
Yujin Lee <yj587878@gmail.com>
2022-07-19 02:22:36 +0900

    [FEAT] 맛선택 태그 노티라벨 추가(#31)

--- Nobar/Scenes/WritingNote/Cell/TastingNoteHeaderView.swift
+++ Nobar/Scenes/WritingNote/Cell/TastingNoteHeaderView.swift
@@ -22,7 +22,7 @@ final class TastingNoteHeaderView: UICollectionReusableView {
     $0.font = Pretendard.size20.black()
   }
   
-  private let subLabel = UILabel().then {
+  private let notiLabel = UILabel().then {
     $0.textColor = Color.pink01.getColor()
     $0.font = Pretendard.size10.medium()
   }
@@ -49,7 +49,7 @@ extension TastingNoteHeaderView {
     }
   }
   
-  func configUI(type: TastingNoteHeaderType) {
+  private func configUI(type: TastingNoteHeaderType) {
     switch type {
     case .cocktail:
       titleLabel.text = "기록하고 싶은 칵테일을 알려주세요"
@@ -57,12 +57,22 @@ extension TastingNoteHeaderView {
       titleLabel.text = "칵테일을 만든 날짜를 알려주세요"
     case .taste:
       titleLabel.text = "맛은 어땠나요?"
+      notiLabel.text = "최대 3개까지 선택 가능합니다"
+      self.addNotiLabel()
     case .score:
       titleLabel.text = "제 점수는요!"
     case .evaluation:
       titleLabel.text = "자세한 평가를 듣고 싶어요"
     case .experience:
       titleLabel.text = "당신의 경험을 기억하고 싶어요"
+    }
+  }
+  
+  private func addNotiLabel() {
+    addSubview(notiLabel)
+    notiLabel.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.trailing.equalToSuperview().inset(26)
     }
   }
 }
