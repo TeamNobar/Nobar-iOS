53655326a679e15253f40d1bdf283a5ca805df24
elesahich <elesahich@gmail.com>
2022-07-18 14:39:16 +0900

    [FEAT] 테이스팅 노트, 나중에 만들 레시피 뷰 더미 작성

---
+++ Nobar/Scenes/MyPage/PageViewContents/MakeOnLaterRecipesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MakeOnLaterRecipesViewController.swift
+//  Nobar
+//
+//  Created by Ian on 2022/07/18.
+//
+
+import UIKit
+
+import RxSwift
+
+final class MakeOnLaterRecipesViewController: BaseViewController {  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.view.backgroundColor = .systemGray.withAlphaComponent(0.4)
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    
+  }
+}
