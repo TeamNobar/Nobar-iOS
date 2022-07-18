594badcf91fbef741f221bcee1b256202ddad093
Yujin Lee <yj587878@gmail.com>
2022-07-19 02:09:01 +0900

    [FEAT] TasteCVC 구현(#31)

--- Nobar/Scenes/WritingNote/Cell/TasteCVC.swift
+++ Nobar/Scenes/WritingNote/Cell/TasteCVC.swift
@@ -8,18 +8,45 @@
 import UIKit
 
 class TasteCVC: UICollectionViewCell {
-  private let stackView = UIStackView()
+  
+  private let dummyPhraseList = ["맛이좋아요","맛이쏘쏘쏘","아주별로야","네번쨰문구","다번째문구","여번째문구","일번째문구","여번쨰문구","아번째문구"]
+  private var tagViews: [TastingTagView] = []
+  
+  private let wholeStackView = UIStackView().then{
+    $0.axis = .vertical
+    $0.spacing = 10
+    $0.backgroundColor = .white
+  }
+  
+  private let topStackView = UIStackView().then{
+    $0.axis = .horizontal
+    $0.spacing = 8
+    $0.backgroundColor = .white
+  }
+  private let middleStackView = UIStackView().then{
+    $0.axis = .horizontal
+    $0.spacing = 8
+    $0.backgroundColor = .white
+  }
+  private let bottomStackView = UIStackView().then{
+    $0.axis = .horizontal
+    $0.spacing = 8
+    $0.backgroundColor = .white
+  }
+
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+    bind()
     render()
+    
   }
   
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-    
+  
 }
 
 // MARK: - UI & Layout
@@ -27,14 +54,42 @@ class TasteCVC: UICollectionViewCell {
 extension TasteCVC {
   
   private func render() {
-    addSubview(stackView)
+    addSubview(wholeStackView)
     
-    stackView.snp.makeConstraints {
+    wholeStackView.addArrangedSubview(topStackView)
+    wholeStackView.addArrangedSubview(middleStackView)
+    wholeStackView.addArrangedSubview(bottomStackView)
+    
+    wholeStackView.snp.makeConstraints{
       $0.centerX.centerY.equalToSuperview()
     }
     
   }
   
+  private func bind() {
+    
+    for i in 0..<9 {
+      let tagView = TastingTagView()
+      tagView.setTasteLabel(with: dummyPhraseList[i])
+      tagView.tag = i
+      tagViews += [tagView]
+      switch i {
+      case 0..<3: topStackView.addArrangedSubview(tagView)
+      case 3..<6: middleStackView.addArrangedSubview(tagView)
+      case 6..<9: bottomStackView.addArrangedSubview(tagView)
+      default: return
+      }
+      
+      let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.didTapTagView(sender:)))
+      tagView.addGestureRecognizer(gesture)
+    }
+  }
+  
+  
+  @objc private func didTapTagView(sender: UITapGestureRecognizer) {
+    print("TagView didTapped")
+    print(sender.view?.tag)
+  }
 }
 
 
---
+++ Nobar/Scenes/WritingNote/Component/TastingTagView.swift
@@ -0,0 +1,63 @@
+//
+//  TastingTagView.swift
+//  Nobar
+//
+//  Created by 이유진 on 2022/07/18.
+//
+
+import UIKit
+
+class TastingTagView: UIView {
+  
+  private var tastePhrase = "맛이 좋아요"
+
+  private let iconImageView = UIImageView().then{
+    $0.image = ImageFactory.tagIconGray
+  }
+  
+  private let tasteLabel = UILabel().then {
+    $0.textColor = Color.black.getColor()
+    $0.font = Pretendard.size13.bold()
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setLayout()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setLayout() {
+    addSubviews([iconImageView,tasteLabel])
+    
+    backgroundColor = Color.gray01.getColor()
+    layer.cornerRadius = 100
+    tasteLabel.text = tastePhrase
+    
+    self.snp.makeConstraints({
+      $0.width.equalTo(102)
+      $0.height.equalTo(40)
+    })
+    
+    iconImageView.snp.makeConstraints{
+      $0.centerX.equalToSuperview().offset(-40)
+      $0.centerY.equalToSuperview()
+    }
+    
+    tasteLabel.snp.makeConstraints{
+      $0.leading.equalTo(iconImageView.snp.trailing).offset(6)
+      $0.centerY.equalToSuperview()
+    }
+    
+  }
+  func setTasteLabel(with phrase: String){
+    self.tasteLabel.text = phrase
+    self.tastePhrase = phrase
+  }
+  
+}
+
+
