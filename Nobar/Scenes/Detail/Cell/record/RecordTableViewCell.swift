ec998bef03b3621a81352d8dec5c7a3bc7dfa12b
suyeon <rlatndus9911@naver.com>
2022-07-22 08:11:28 +0900

    [FEAT] 디테일 ui 수정

--- Nobar/Scenes/Detail/Cell/record/RecordTableViewCell.swift
+++ Nobar/Scenes/Detail/Cell/record/RecordTableViewCell.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 class RecordTableViewCell: UITableViewCell {
 
     @IBOutlet weak var contentLabel: UILabel!
-    @IBOutlet weak var writeNoteButton: UIButton!
+
+  private lazy var writeNoteButton = UIButton().then {
+    $0.setTitle("테이스팅 노트 작성하기", for: .normal)
+    $0.setTitleColor(Color.gray04.getColor(), for: .normal)
+    $0.titleLabel?.font = Pretendard.size13.bold()
+    $0.titleLabel?.textAlignment = .center
+    $0.layer.cornerRadius = 20
+    $0.layer.borderWidth = 0.5
+    $0.layer.borderColor = Color.gray02.getColor().cgColor
+  }
     
     static let identifier = "RecordTableViewCell"
     
@@ -20,13 +32,15 @@ class RecordTableViewCell: UITableViewCell {
       contentLabel.text = "같은 칵테일을 마셔도 테이스팅 노트는 여러 번 기록할 수 있어요\n어제의 홈텐딩과 오늘의 홈텐딩은 다르니까요\n오늘 마신 칵테일로 오늘을 기억해보는 것은 어때요?\n당신의 하루가 더 오래 기억되길 바라요"
       contentLabel.font = Pretendard.size12.light()
       contentLabel.textColor = Color.black.getColor()
+      contentLabel.setAttributedText(defaultText: contentLabel.text ?? "", kernValue: -0.24, lineSpacing: 4)
 
-      writeNoteButton.setTitle("테이스팅 노트 작성하기", for: .normal)
-      writeNoteButton.setTitleColor(Color.gray04.getColor(), for: .normal)
-      writeNoteButton.layer.cornerRadius = 20
-      writeNoteButton.layer.borderWidth = 0.5
-      writeNoteButton.layer.borderColor = Color.gray02.getColor().cgColor
-      
+      addSubview(writeNoteButton)
+      writeNoteButton.snp.makeConstraints {
+        $0.top.equalTo(contentLabel.snp.bottom).offset(18)
+        $0.centerX.equalToSuperview()
+        $0.width.equalTo(171)
+        $0.height.equalTo(40)
+      }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
