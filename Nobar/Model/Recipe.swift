d284f2550776e487108de8e36936a795a52306b6
elesahich <elesahich@gmail.com>
2022-07-21 19:06:32 +0900

    [FEAT] 모델 변경점 일부 반영

--- Nobar/Model/Recipe.swift
+++ Nobar/Model/Recipe.swift
@@ -9,11 +9,14 @@ struct Recipe: Decodable {
   let id: String
   var name: String
   var enName: String
-  var version: [String]
-  var base: BaseRecipe
+  var version: [String]?
+  var base: BaseRecipe?
+  var defaultRecipes: String?
   var proof: Int
   var skill: Skill
   var glass: Glass
   var ingredients: [Ingredient]
   var steps: [String]
 }
+
+
