1581e6ae768f33acfb5e01d570543596d7224a71
hansua <hansua990928@gmail.com>
2022-07-22 00:03:08 +0900

    [ADD] HeaderRecordTVC UI설정

--- Nobar/Scenes/Detail/Cell/headerRecord/HeaderRecordTableViewCell.swift
+++ Nobar/Scenes/Detail/Cell/headerRecord/HeaderRecordTableViewCell.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class HeaderRecordTableViewCell: UITableViewCell {
     
-    static let identifier = "HeaderRecordTableViewCell"
+  @IBOutlet weak var headerTitleLabel: UILabel!
+  
+  static let identifier = "HeaderRecordTableViewCell"
 
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+      super.awakeFromNib()
+      // Initialization code
+      headerTitleLabel.text = "오늘의 이 경험을, 기록해볼까요?"
+      headerTitleLabel.font = Pretendard.size18.bold()
+
+      let attributedStr = NSMutableAttributedString(string: headerTitleLabel.text!)
+      attributedStr.addAttribute(.foregroundColor, value: Color.black.getColor(), range: (headerTitleLabel.text! as NSString).range(of: "오늘의 이 경험을,"))
+      attributedStr.addAttribute(.foregroundColor, value: Color.navy01.getColor(), range: (headerTitleLabel.text! as NSString).range(of: "기록해볼까요?"))
+
+      headerTitleLabel.attributedText = attributedStr
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,5 +31,4 @@ class HeaderRecordTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
 }
