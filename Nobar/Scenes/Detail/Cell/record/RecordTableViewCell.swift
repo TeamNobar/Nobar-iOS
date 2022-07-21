7a809fb914789a0cb53a583bfa17ce1e3d1d440b
hansua <hansua990928@gmail.com>
2022-07-22 01:22:41 +0900

    [ADD] ingredientCVC, RecordTVC UI 추가

--- Nobar/Scenes/Detail/Cell/record/RecordTableViewCell.swift
+++ Nobar/Scenes/Detail/Cell/record/RecordTableViewCell.swift
@@ -15,8 +15,18 @@ class RecordTableViewCell: UITableViewCell {
     static let identifier = "RecordTableViewCell"
     
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+      super.awakeFromNib()
+      // Initialization code
+      contentLabel.text = "같은 칵테일을 마셔도 테이스팅 노트는 여러 번 기록할 수 있어요\n어제의 홈텐딩과 오늘의 홈텐딩은 다르니까요\n오늘 마신 칵테일로 오늘을 기억해보는 것은 어때요?\n당신의 하루가 더 오래 기억되길 바라요"
+      contentLabel.font = Pretendard.size12.light()
+      contentLabel.textColor = Color.black.getColor()
+
+      writeNoteButton.setTitle("테이스팅 노트 작성하기", for: .normal)
+      writeNoteButton.setTitleColor(Color.gray04.getColor(), for: .normal)
+      writeNoteButton.layer.cornerRadius = 20
+      writeNoteButton.layer.borderWidth = 0.5
+      writeNoteButton.layer.borderColor = Color.gray02.getColor().cgColor
+      
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
