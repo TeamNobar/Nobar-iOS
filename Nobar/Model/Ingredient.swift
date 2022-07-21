fbb7f4a285dc85d486b3883a5874f9ad4ea8a26e
suyeon <rlatndus9911@naver.com>
2022-07-22 03:09:11 +0900

    [FEAT] 모델 Hashable 프로토콜 채택

--- Nobar/Model/Ingredient.swift
+++ Nobar/Model/Ingredient.swift
@@ -5,11 +5,11 @@
 //  Created by Ian on 2022/07/13.
 //
 
-struct Ingredient: Decodable {
+struct Ingredient: Decodable, Hashable {
   let id: String
   var name: String
   var enName: String
   var proof: Int
   var category: String
-  var quantity: String
+  var quantity: String?
 }
