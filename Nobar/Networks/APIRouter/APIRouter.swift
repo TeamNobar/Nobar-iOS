c6cf41776c0f16391101d08f54f4bdf03d51a8e1
elesahich <elesahich@gmail.com>
2022-07-22 22:56:52 +0900

    [FIX] 빌드 이슈 해결

--- Nobar/Networks/APIRouter/APIRouter.swift
+++ Nobar/Networks/APIRouter/APIRouter.swift
@@ -56,9 +56,9 @@ enum APIRouter {
   var headers: HTTPHeaders {
     switch self {
     case .getMyPage,
-         .writeTastingNote
+        .writeTastingNote:
       return [
-          "Content-type": "application/json"
+          "Content-type": "application/json",
           "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkOWRkYmFkYmI1NDBiODhjZDVlZGM4In0sImlhdCI6MTY1ODQ3NjAwMCwiZXhwIjo0NzgyNjc4NDAwfQ.PDvodnxCJrq8XTxoZIcYdNheK9FSB-wjjfe2_t8-D-Q"
       ]
 
